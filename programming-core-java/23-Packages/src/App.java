871dbba378839a4775c5ab66ad9e9981bcc98b10
ChristianBuado <32333101+masterblueboy@users.noreply.github.com>
2018-12-07 09:00:40 +0800

    Done with Package

---
+++ programming-core-java/23-Packages/src/App.java
@@ -0,0 +1,14 @@
+// import Fish class from 'ocean' package
+import ocean.Fish;
+import ocean.plants.Seaweed;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Fish fish = new Fish();
+		Seaweed weed = new Seaweed();
+	}
+
+}
